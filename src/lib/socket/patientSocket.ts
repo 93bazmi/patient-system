ba124f645b8de6900ff8cbb4ab9cc147e3d8fb73
93bazmi <bambamboom234@gmail.com>
2026-04-29 03:31:24 +0700

    Remove unused patient and staff socket files

--- src/lib/socket/patientSocket.ts
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-
-export const patientSocket = io("http://localhost:3001/patient", {
-  autoConnect: false,
-});
--- src/lib/socket/staffSocket.ts
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-
-export const staffSocket = io("http://localhost:3001/staff", {
-  autoConnect: false,
-});
