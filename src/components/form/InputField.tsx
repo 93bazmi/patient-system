37aa977eae0ca9f0c58921f4b5c69d4560d2db44
93bazmi <bambamboom234@gmail.com>
2026-04-29 04:15:24 +0700

    Improve InputField styling by adding min-width and overflow properties

--- src/components/form/InputField.tsx
+++ src/components/form/InputField.tsx
@@ -84,7 +84,7 @@ focus:ring-2 focus:ring-blue-200 focus:outline-none focus:bg-white ${
               : props.value
           }
           disabled={disabled}
-          className={`h-11 w-full bg-blue-50 rounded-lg px-3 py-2 border 
+          className={`h-11 w-full min-w-0 overflow-hidden bg-blue-50 rounded-lg px-3 py-2 border 
           ${error ? "border-red-400" : "border-transparent"}
           focus:bg-white focus:ring-2 focus:ring-blue-100 focus:outline-none transition ${
             icon ? "pl-12" : ""
